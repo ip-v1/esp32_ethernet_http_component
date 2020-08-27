c9907f21491844603d81f6a3c6e30e21fd6ca8ff
Inderpreet Singh <ip_v1@hotmail.com>
2020-08-27 11:35:28 -0400

    Add I2c GPIO and blinky code to uart.h uart.c

--- components/uart_comms/uart_comms.c
+++ components/uart_comms/uart_comms.c
@@ -12,6 +12,9 @@ InfoT infoT;
 
 void vTaskCode(void *pvParameters) {
   // uint8_t data* = malloc(1024);
+  gpio_reset_pin(SCL1_PIN);
+  gpio_set_direction(SCL1_PIN, GPIO_MODE_OUTPUT);
+
   for (;;) {
     static uint8_t state = 0;
     uart_config_t uart_config = {
@@ -33,7 +36,10 @@ void vTaskCode(void *pvParameters) {
     case 1: // work state
       // len = uart_read_bytes(UART_NUM_2, data, 1024, 20/portTICK_RATE_MS);
       uart_write_bytes(UART_NUM_1, (const char *)msg, 13);
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+      gpio_set_level(SCL1_PIN, 1);
+      vTaskDelay(500 / portTICK_PERIOD_MS);
+      gpio_set_level(SCL1_PIN, 0);
       break;
     default:
       break;
--- components/uart_comms/uart_comms.h
+++ components/uart_comms/uart_comms.h
@@ -5,8 +5,11 @@
 #include "driver/uart.h"
 
 #define U1_TXD (GPIO_NUM_4)
-#define U1_RXD (GPIO_NUM_2)
+#define U1_RXD (GPIO_NUM_5)
 #define NC (UART_PIN_NO_CHANGE)
+
+#define SCL1_PIN GPIO_NUM_12
+#define SDA1_PIN GPIO_NUM_14
 
 void start_task_test(void);
 
